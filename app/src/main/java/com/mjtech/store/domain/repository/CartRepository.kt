a273a6c9a60922a99e3a34bf4fc0e1b35dc0d365
Matheus Ferreira <matheusjuan.f@outlook.com>
2025-10-17 10:23:17 -0300

    ✨ feat: add interface repository for cart

--- app/src/main/java/com/mjtech/store/domain/repository/CartRepository.kt
+++ app/src/main/java/com/mjtech/store/domain/repository/CartRepository.kt
@@ -1,8 +1,13 @@
 package com.mjtech.store.domain.repository
 
+import com.mjtech.store.domain.common.DataResult
+import com.mjtech.store.domain.model.Product
+import kotlinx.coroutines.flow.Flow
+
 interface CartRepository {
 
-    suspend fun addItem(productId: String)
-    suspend fun removeItem(productId: String)
-    suspend fun clearCart()
+    fun addItem(productId: String): Flow<DataResult<Unit>>
+    fun removeItem(productId: String): Flow<DataResult<Unit>>
+    fun clearCart(): Flow<DataResult<Unit>>
+    fun getCartItems(): Flow<DataResult<List<Product>>>
 }
