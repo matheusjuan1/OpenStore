b81b070b8b8fff8acf582d2e72753e217f99ede7
Matheus Ferreira <matheusjuan.f@outlook.com>
2025-10-20 09:25:57 -0300

    ✨ feat: implement getCartMetrics to calculate total price and item counts in CartViewModel

--- app/src/main/java/com/mjtech/store/ui/cart/CartViewModel.kt
+++ app/src/main/java/com/mjtech/store/ui/cart/CartViewModel.kt
@@ -7,6 +7,10 @@ import com.mjtech.store.domain.model.Product
 import com.mjtech.store.domain.repository.CartRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
@@ -14,6 +18,10 @@ class CartViewModel(private val cartRepository: CartRepository) : ViewModel() {
 
     private val _cartUiState = MutableStateFlow(CartUiState())
     val cartUiState: StateFlow<CartUiState> = _cartUiState
+
+    init {
+        getCartMetrics()
+    }
 
     fun onAddProductToCart(product: Product) {
         viewModelScope.launch {
@@ -45,5 +53,41 @@ class CartViewModel(private val cartRepository: CartRepository) : ViewModel() {
         _cartUiState.update { currentState ->
             currentState.copy(removeItemState = DataResult.Success(Unit))
         }
+    }
+
+    private fun getCartMetrics() {
+        // Total Price
+        val totalPriceFlow = cartRepository.getTotalPrice()
+            .map { result ->
+                if (result is DataResult.Success) result.data else 0.0
+            }
+            .catch { emit(0.0) }
+
+        // Total items count
+        val itemsMapFlow = cartRepository.getCartItems()
+            .map { result ->
+                if (result is DataResult.Success) {
+                    result.data.associate { it.product.id to it.quantity }
+                } else {
+                    emptyMap()
+                }
+            }
+            .catch { emit(emptyMap()) }
+
+        // Combine flows
+        totalPriceFlow.combine(itemsMapFlow) { price, quantitiesMap ->
+            val totalItemsCount = quantitiesMap.values.sum()
+
+            // Quantity per product
+            val quantitiesProducts = quantitiesMap.mapKeys { it.key }
+
+            _cartUiState.update { currentState ->
+                currentState.copy(
+                    totalPrice = price,
+                    totalItemsCount = totalItemsCount,
+                    quantitiesProducts = quantitiesProducts
+                )
+            }
+        }.launchIn(viewModelScope)
     }
 }
