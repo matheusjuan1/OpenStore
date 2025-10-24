b31bd48f49a8260cb66fc27460736297ee91b582
Matheus Ferreira <matheusjuan.f@outlook.com>
2025-10-24 14:09:21 -0300

    ✨ feat: add Onboarding screen and navigation

---
+++ app/src/main/java/com/mjtech/store/ui/onboarding/OnboardingActivity.kt
@@ -0,0 +1,42 @@
+package com.mjtech.store.ui.onboarding
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.mjtech.store.R
+import com.mjtech.store.databinding.ActivityOnboardingBinding
+import com.mjtech.store.ui.products.ProductsActivity
+
+class OnboardingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnboardingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.cardWeighDish.setOnClickListener {
+            // TODO
+        }
+
+        binding.cardComplementaryProducts.setOnClickListener {
+            Intent(this, ProductsActivity::class.java).apply {
+                startActivity(this)
+            }
+        }
+    }
+}
