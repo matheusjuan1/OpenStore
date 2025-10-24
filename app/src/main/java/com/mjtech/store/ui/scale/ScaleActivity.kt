28c778911c70c7b1ed63ff83373f74f2e9b99681
Matheus Ferreira <matheusjuan.f@outlook.com>
2025-10-24 16:45:55 -0300

    ✨ feat: add ScaleActivity for dish weighing`.

--- app/src/main/java/com/mjtech/store/ui/scale/ScaleActivity.kt
+++ app/src/main/java/com/mjtech/store/ui/scale/ScaleActivity.kt
@@ -1,23 +1,20 @@
-package com.mjtech.store.ui.onboarding
+package com.mjtech.store.ui.scale
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.mjtech.store.R
-import com.mjtech.store.databinding.ActivityOnboardingBinding
-import com.mjtech.store.ui.products.ProductsActivity
-import com.mjtech.store.ui.scale.ScaleActivity
+import com.mjtech.store.databinding.ActivityScaleBinding
 
-class OnboardingActivity : AppCompatActivity() {
+class ScaleActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityOnboardingBinding
+    private lateinit var binding: ActivityScaleBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        binding = ActivityScaleBinding.inflate(layoutInflater)
         setContentView(binding.root)
         enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -30,16 +27,8 @@ class OnboardingActivity : AppCompatActivity() {
     }
 
     private fun initListeners() {
-        binding.cardWeighDish.setOnClickListener {
-            Intent(this, ScaleActivity::class.java).apply {
-                startActivity(this)
-            }
-        }
-
-        binding.cardComplementaryProducts.setOnClickListener {
-            Intent(this, ProductsActivity::class.java).apply {
-                startActivity(this)
-            }
+        binding.fabBack.setOnClickListener {
+            finish()
         }
     }
 }
